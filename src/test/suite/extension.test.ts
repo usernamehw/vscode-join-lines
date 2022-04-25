98ebb9697923ced66e98ec9020eac060ea608a1d
al <usernamehw@gmail.com>
2022-04-25 19:05:24 +0300

    test: add test for indented text

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -46,80 +46,99 @@ const multipleCursorSelections = [
 	new Selection(1, 0, 1, 0),
 	new Selection(3, 0, 3, 0),
 	new Selection(4, 0, 4, 0),
-]
+];
+// ────────────────────────────────────────────────────────────
+const withIndentDocument =
+`    one
+    two
+    three`;
+const withIndentSelection = new Selection(0, 0, 2, 0);
 // ────────────────────────────────────────────────────────────
 
 describe('Basic test', () => {
-	before(async() => {
+	before(async () => {
 		await commands.executeCommand('workbench.action.closeEditorsInGroup');
 		await openInUntitled(simplestDocument, 'plaintext');
 	});
 
-	it('Simple join 2 lines', async() => {
+	it('Simple join 2 lines', async () => {
 		window.activeTextEditor!.selection = simplestDocumentSelection;
-		await commands.executeCommand(CommandId.joinLines);
+		await commands.executeCommand(CommandId.joinLinesCommand);
 		await delay(100);
 		assert.equal(getActiveEditorText(), 'one two');
 	});
 });
 
 describe('With empty lines', () => {
-	before(async() => {
+	before(async () => {
 		await replaceActiveEditorDocumentContent(window.activeTextEditor!, withEmptySpaceInBetweenDocument);
 	});
 
-	it('Removes multiple empty lines', async() => {
+	it('Removes multiple empty lines', async () => {
 		window.activeTextEditor!.selection = emptySpaceDocumentSelection;
-		await commands.executeCommand(CommandId.joinLines);
+		await commands.executeCommand(CommandId.joinLinesCommand);
 		await delay(100);
 		assert.equal(getActiveEditorText(), 'one two');
 	});
 });
 
 describe('With changed separator', () => {
-	beforeEach(async() => {
+	beforeEach(async () => {
 		await replaceActiveEditorDocumentContent(window.activeTextEditor!, simplestDocument);
 	});
 
-	it('Applies different line separator (from settings.json)', async() => {
+	it('Applies different line separator (from settings.json)', async () => {
 		window.activeTextEditor!.selection = simplestDocumentSelection;
-		let settings = workspace.getConfiguration("joinLines");
+		const settings = workspace.getConfiguration('joinLines');
 		await settings.update('defaultSeparator', '_', ConfigurationTarget.Global);
-		await commands.executeCommand(CommandId.joinLines);
+		await commands.executeCommand(CommandId.joinLinesCommand);
 		await delay(100);
 		assert.equal(getActiveEditorText(), 'one_two');
 		await settings.update('defaultSeparator', ' ', ConfigurationTarget.Global);
 	});
 
-	it('Applies different line separator provided as an argument', async() => {
-		await commands.executeCommand(CommandId.joinLines, { separator: '0000' });
+	it('Applies different line separator provided as an argument', async () => {
+		await commands.executeCommand(CommandId.joinLinesCommand, { separator: '0000' });
 		await delay(100);
 		assert.equal(getActiveEditorText(), 'one0000two');
 	});
 });
 
 describe('Multiple non-empty selections', () => {
-	before(async() => {
+	before(async () => {
 		await replaceActiveEditorDocumentContent(window.activeTextEditor!, multipleSelectionsDocument);
 	});
 
-	it('Works on multiple selections', async() => {
+	it('Works on multiple selections', async () => {
 		window.activeTextEditor!.selections = multipleSelections;
-		await commands.executeCommand(CommandId.joinLines);
+		await commands.executeCommand(CommandId.joinLinesCommand);
 		await delay(100);
-		assert.equal(getActiveEditorText(), 'one two\n\nthree four')
+		assert.equal(getActiveEditorText(), 'one two\n\nthree four');
 	});
 });
 
 describe('Multiple empty selections(cursors)', () => {
-	before(async() => {
+	before(async () => {
 		await replaceActiveEditorDocumentContent(window.activeTextEditor!, multipleCursorsWithEmptySelectionDocument);
 	});
 
-	it('Joins multiple consequtive lines with empty selections(cursors)', async() => {
+	it('Joins multiple consequtive lines with empty selections(cursors)', async () => {
 		window.activeTextEditor!.selections = multipleCursorSelections;
-		await commands.executeCommand(CommandId.joinLines);
+		await commands.executeCommand(CommandId.joinLinesCommand);
 		await delay(100);
-		assert.equal(getActiveEditorText(), 'one two\n\nthree four')
-	})
+		assert.equal(getActiveEditorText(), 'one two\n\nthree four');
+	});
+});
+
+describe('With indent', () => {
+	before(async () => {
+		await replaceActiveEditorDocumentContent(window.activeTextEditor!, withIndentDocument);
+	});
+
+	it('Removes indent (except first line)', async () => {
+		window.activeTextEditor!.selection = withIndentSelection;
+		await commands.executeCommand(CommandId.joinLinesCommand);
+		await delay(100);
+		assert.equal(getActiveEditorText(), '    one two three');
+	});
 });
